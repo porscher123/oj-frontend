73b2ed3d337aa5a800c1dfceb9f5ec982b3e2018
Mi    tiepinuanhu <13236388+mi-tiepinuanhu@user.noreply.gitee.com>
2024-02-25 21:16:59 +0800

    顶部导航栏, logo, 基础布局, vue组件引用

---
+++ src/layouts/BasicLayout.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="basicLayout">
+    <a-layout style="height: 400px;">
+      <a-layout-header class="header">
+        <GlobalHeader></GlobalHeader>
+      </a-layout-header>
+
+     
+      <a-layout-content class="content">
+        <router-view/>
+      </a-layout-content>
+        
+
+      <a-layout-footer class="footer">
+        super OJ by xinchao wang
+      </a-layout-footer>
+    </a-layout>
+  </div>
+</template>
+<script>
+  import GlobalHeader from '@/components/GlobalHeader.vue'
+  export default {
+    components: {
+      GlobalHeader
+    }
+  }
+</script>
+
+<style scoped>
+
+#basicLayout .header {
+  background: red;
+  margin-bottom: 16px;
+}
+
+#basicLayout .content {
+  background: linear-gradient(to right, #edf, #dff);
+  margin-bottom: 16px;
+}
+
+#basicLayout .footer {
+  background: blanchedalmond;
+  padding: 16px;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  text-align: center;
+}
+
+</style>
+
+
